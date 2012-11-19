a78e1bd6ad9b44610fecb1599463fec818cf7ca5
MatthewEnderle <mattenderle488@gmail.com>
2012-11-19 10:34:32 -0600

    Cleaned up Whitespace

--- index.php
+++ index.php
@@ -20,46 +20,46 @@
 		</div>
 		<div id="inner">
 			<div id="page">
-					
-						<div id="content">
-							<div>
-								<?php 
-									$sql = "SELECT * FROM content"; //id, title , content
-									$result = mysql_query($sql);
-										
-									if(!$result)
-									{
-										echo 'The server could not be reached, please try again later.' . mysql_error();
-									}
-									else
-									{
-										echo '<table style="width: 650px;">';
-										while($row = mysql_fetch_assoc($result))
-										{
-												echo '<tr>';
-													echo '<td id="title" style="padding-top: 30px; width: 100px;">';
-													echo $row['title'];
-													echo '</td>';
-												echo '</tr>';
-												echo '<tr>';
-	
-													echo '<td style="padding-top: 30px;">';
-													echo $row['content'];
-													echo '</td>';
-												echo '</tr>';
-										}
-										echo '</table>';
-									}
-								?>
-	
-							</div>
-								</table>
-								<br />	
-								
-									</div style="padding: 1em;">
-					
-				</div>
 		
+			<div id="content">
+				<div>
+					<?php 
+						$sql = "SELECT * FROM content"; //id, title , content
+						$result = mysql_query($sql);
+							
+						if(!$result)
+						{
+							echo 'The server could not be reached, please try again later.' . mysql_error();
+						}
+						else
+						{
+							echo '<table style="width: 650px;">';
+							while($row = mysql_fetch_assoc($result))
+							{
+									echo '<tr>';
+										echo '<td id="title" style="padding-top: 30px; width: 100px;">';
+										echo $row['title'];
+										echo '</td>';
+									echo '</tr>';
+									echo '<tr>';
+	
+										echo '<td style="padding-top: 30px;">';
+										echo $row['content'];
+										echo '</td>';
+									echo '</tr>';
+							}
+							echo '</table>';
+						}
+					?>
+
+				</div>
+				</table>
+				<br />	
+					
+			</div>
+		
+		</div>
+
 	</div>
 </body>
 </html>
